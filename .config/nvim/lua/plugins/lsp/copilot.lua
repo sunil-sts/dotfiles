dedfb114f23d70df979ddc68d191228117f2944b
Linus Sundkvist <lsundkvist1@gmail.com>
2023-08-13 05:49:32 +0200

    Yet more cleanup

--- .config/nvim/lua/plugins/lsp/copilot.lua
+++ .config/nvim/lua/plugins/lsp/copilot.lua
@@ -35,7 +35,6 @@ return {
         end,
       },
     },
-    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, 1, { name = "copilot", group_index = 2 })
       opts.sorting = opts.sorting or require("cmp.config.default")().sorting
